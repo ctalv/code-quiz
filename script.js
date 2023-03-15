7368c393b0c37626a35422874f6bb2827e132a6e
ctalv <122413805+ctalv@users.noreply.github.com>
2023-03-15 13:40:34 -0500

    added start page pcode

---
+++ script.js
@@ -0,0 +1,11 @@
+// function init() to describe start page which will have
+    // title
+    // description
+    // button 'start quiz'
+
+// click event for start quiz
+    // timer in click event
+    // pulls question function
+
+// init() at bottom to call start page
+ 
