178dfd8af59590a2ad824f87707c3232999b990f
ctalv <122413805+ctalv@users.noreply.github.com>
2023-03-15 13:49:15 -0500

    added pcode for answer function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// declare root variable
+// declare title page variable
+// declare question variable
+
 // function init() to describe start page which will have
     // title
     // description
@@ -9,6 +13,26 @@
 // click event for start quiz
     // timer in click event
     // pulls question function
+
+// function questions()
+    // index through preset quesions
+    //
+
+// function answer() 
+    // click event on the selected answer
+    // if user select correct
+        // logs score
+    // else 
+        // takes off 15 seconds from clock
+    // continues to next question (not in this function)
+
+// function endGame() to
+    // tell user score
+    // allow user to add initials
+    // click event on submit
+        // stores score
+        // stores associated initals 
+        // without erasing previous
 
 // init() at bottom to call start page
  
