b5ad4dbd6855687d49a4a20b8d8942048ee1f7b8
ctalv <122413805+ctalv@users.noreply.github.com>
2023-03-15 14:03:57 -0500

    added pcode for question function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // declare root variable
 // declare title page variable
 // declare question variable
+// declare counter variable
 
 // function init() to describe start page which will have
     // title
@@ -11,20 +12,29 @@
     // local storage of high score
 
 // click event for start quiz
-    // timer in click event
-    // pulls question function
+    // timer starts
+    // pulls questions() function
 
 // function questions()
-    // index through preset quesions
-    //
+    // index through preset questions with for loop (can be in order; will add random later)
+        // var currentQuestion = objectQuestions[i] 
+        // display question using html = currentQuestion
+        // display options
+        // use append
+        // at options have click events on each 
+        // pull answer() function
+    // counter
 
 // function answer() 
-    // click event on the selected answer
+    // click event on the selected answer (not in this function)
     // if user select correct
         // logs score
+        // sets variable to correct
     // else 
         // takes off 15 seconds from clock
+        // sets variable to wrong
     // continues to next question (not in this function)
+    // display answer below (now new) question
 
 // function endGame() to
     // tell user score
